1308a02def0b9602791b066e3a1cc7a29d70b001
Katherine Gershfeld <ka.gershfeld@gmail.com>
2016-03-28 23:23:17 -0400

    cancer

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ from matrix import *
 import sys
 
 screen = new_screen()
-color = [ 0, 255, 0 ]
+color = [ 0, 255, 255 ]
 edges = []
 transform = new_matrix()
 
 if len(sys.argv) == 2:
     f = open(sys.argv[1])
 else:
-    f = open('script_3d')
+    f = open('script_mine')
 
 parse_file( f, edges, transform, screen, color )
 f.close()
